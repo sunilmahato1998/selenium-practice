1c48936c2c3d2913e6aa8b38c1a8107e9d9abcdd
Sunil Kumar <sunilmahato123456789@gmail.com>
2026-04-14 16:36:59 +0530

    Update WaitDemo.py

--- WaitDemo.py
+++ WaitDemo.py
@@ -1,8 +1,37 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
+
+driver.find_element(By.CSS_SELECTOR, ".search-keyword").send_keys("ber")
+time.sleep(2)
+
+results = driver.find_elements(By.XPATH, "//div[@class='products'] /div")
+count = len(results)
+print(count)
+assert count > 0
+
+for result in results:
+    result.find_element(By.XPATH, "div/button").click()
+
+driver.find_element(By.CSS_SELECTOR, "img[alt = 'Cart']").click()
+driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+
+# FIX: Wait for promo code field
+wait = WebDriverWait(driver, 10)
+promo_code_field = wait.until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, ".promoCode"))
+)
+promo_code_field.send_keys("rahulshettyacademy")
+
+driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+time.sleep(5)
+
+print(driver.find_element(By.CSS_SELECTOR, ".promoInfo").text)
+
+driver.quit()
